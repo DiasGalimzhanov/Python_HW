9c3e53ceec94e88bc3693051aa44f4d76035af88
DiasGalimzhanov <126086058+DiasGalimzhanov@users.noreply.github.com>
2023-12-23 00:05:01 +0600

    Homework try: exept:

--- Python_HW.py
+++ Python_HW.py
@@ -1,1 +1,21 @@
 
+
+def plus_two(number):  
+    try:   
+        print(number + 2)   
+    except:        
+        print("The expected data type is a number")
+        
+plus_two("a")
+
+
+def idx_out():
+    mas = [1,5,4,7,9,3]   
+    try:       
+        for i in len(mas):            
+            print(mas[i])   
+    except:         
+        print("Index out array")
+        
+
+idx_out()
